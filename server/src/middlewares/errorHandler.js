3073b424966a1cc352a5ad081bed047738300473
Prakhar7755 <prakhar.5779.aps3@gmail.com>
2026-05-18 20:08:57 +0530

    test: test ci

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 const errorHandler = (err, req, res, _next) => {
   const statusCode = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
 
+  console.log("Hello");
   res.status(statusCode).json({
     success: false,
     message: err.message || "Server Error",
